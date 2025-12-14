9dfc391190fc3264fa56342e950706c59dee45d0
Gugle <administration@ztxy666.cn>
2025-12-14 23:44:37 +0800

    refactor(lucky): 调整幸运方块破坏事件的处理逻辑
    - 从 LuckyBlock 类中移除 @Mod.EventBusSubscriber 注解
    - 删除 LuckyBlock 类中的 onBlockBreak 方法
    - 在 LuckyBlockMod 类中添加 onBlockBreak 方法
    - 调整包导入，移除不再使用的引用

--- src/main/java/dev/dubhe/lucky/LuckyBlockMod.java
+++ src/main/java/dev/dubhe/lucky/LuckyBlockMod.java
@@ -1,14 +1,23 @@
 package dev.dubhe.lucky;
 
 import com.mojang.logging.LogUtils;
+import dev.dubhe.lucky.block.entity.LuckyBlockEntity;
 import dev.dubhe.lucky.init.ModBlocks;
 import dev.dubhe.lucky.init.ModItems;
 import dev.dubhe.lucky.server.ServerLuckyBlockEffectManager;
+import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.AddReloadListenerEvent;
+import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.event.server.ServerStoppedEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.jetbrains.annotations.NotNull;
@@ -30,6 +39,7 @@ public class LuckyBlockMod {
         ModItems.DataComponents.DATA_COMPONENT_TYPES.register(modEventBus);
         MinecraftForge.EVENT_BUS.addListener(this::onResourceReload);
         MinecraftForge.EVENT_BUS.addListener(this::onServerStopped);
+        MinecraftForge.EVENT_BUS.addListener(this::onBlockBreak);
     }
 
     public static @NotNull ResourceLocation location(String path) {
@@ -45,5 +55,19 @@ public class LuckyBlockMod {
 
     public void onServerStopped(@NotNull ServerStoppedEvent event) {
         LuckyBlockMod.luckyBlockEffectManager = null;
+    }
+
+    public void onBlockBreak(@NotNull BlockEvent.BreakEvent event) {
+        LevelAccessor level = event.getLevel();
+        if (!(level instanceof ServerLevel serverLevel)) return;
+        BlockPos pos = event.getPos();
+        BlockState state = level.getBlockState(pos);
+        if (!state.is(ModBlocks.Tags.LUCKY_BLOCK)) return;
+        if (!state.hasBlockEntity()) return;
+        LuckyBlockEntity entity = (LuckyBlockEntity) level.getBlockEntity(pos);
+        if (entity == null) return;
+        MinecraftServer server = serverLevel.getServer();
+        ServerPlayer player = (ServerPlayer) event.getPlayer();
+        entity.onBreak(server, serverLevel, pos, player);
     }
 }
--- src/main/java/dev/dubhe/lucky/block/LuckyBlock.java
+++ src/main/java/dev/dubhe/lucky/block/LuckyBlock.java
@@ -1,23 +1,14 @@
 package dev.dubhe.lucky.block;
 
 import dev.dubhe.lucky.block.entity.LuckyBlockEntity;
-import dev.dubhe.lucky.init.ModBlocks;
 import net.minecraft.core.BlockPos;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.event.level.BlockEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-@Mod.EventBusSubscriber(modid = "lucky")
 public class LuckyBlock extends Block implements EntityBlock {
     private final int defaultLucky;
 
@@ -33,20 +24,5 @@ public class LuckyBlock extends Block implements EntityBlock {
     @Override
     public @Nullable BlockEntity newBlockEntity(@NotNull BlockPos pos, @NotNull BlockState state) {
         return new LuckyBlockEntity(pos, state, this.defaultLucky);
-    }
-
-    @SubscribeEvent
-    public static void onBlockBreak(BlockEvent.@NotNull BreakEvent event) {
-        LevelAccessor level = event.getLevel();
-        if (!(level instanceof ServerLevel serverLevel)) return;
-        BlockPos pos = event.getPos();
-        BlockState state = level.getBlockState(pos);
-        if (!state.is(ModBlocks.Tags.LUCKY_BLOCK)) return;
-        if (!state.hasBlockEntity()) return;
-        LuckyBlockEntity entity = (LuckyBlockEntity) level.getBlockEntity(pos);
-        if (entity == null) return;
-        MinecraftServer server = serverLevel.getServer();
-        ServerPlayer player = (ServerPlayer) event.getPlayer();
-        entity.onBreak(server, serverLevel, pos, player);
     }
 }
